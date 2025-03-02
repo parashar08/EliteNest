23a6ecf07d4347a2660c13bbf96431f663119dd3
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-02 14:19:08 +0530

    add navigation

--- script/header.js
+++ script/header.js
@@ -9,3 +9,19 @@ inputElement.addEventListener('focus', () => {
 inputElement.addEventListener('blur', () => {
     inputBox.classList.remove('input-focus');
 });
+
+// three dot navigation
+
+const threeDot = document.querySelector('.js-three-dot');
+const navigationBox = document.querySelector('.js-nav');
+const xCross = document.querySelector('.js-x-cross');
+
+threeDot.addEventListener('click', () => {
+    navigationBox.classList.add('navigation');
+    navigationBox.classList.remove('remove-nav');
+});
+
+xCross.addEventListener('click', () => {
+    navigationBox.classList.remove('navigation');
+    navigationBox.classList.add('remove-nav');
+})
